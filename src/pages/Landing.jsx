548b11ca3a85e9329e081b1e84308610d8416e51
Daniel Schott <dschott@scu.edu>
2023-03-12 11:05:14 -0700

    A link section for development

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -47,6 +47,23 @@ const Landing = () => {
           <Link to="/contact"><button>Contact</button></Link>
         </div>
       </div>
+      {/* Temporary section for development */}
+      <div className="temp-links">
+        <p>Temporary Links for Development:</p>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/listening">Listening</Link></li>
+          <li><Link to="/hosting">Hosting</Link></li>
+          <li><Link to="/account">Account</Link></li>
+          <li><Link to="/help">Help</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/socials">Socials</Link></li>
+          <li><Link to="/user">User</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/create-account">Create Account</Link></li>
+        </ul>
+      </div>
+      {/* End of temporary section */}
     </div>
   );
 };
