431ed7910f3db2d5ad18b58876b241364097b948
Daniel Schott <dschott@scu.edu>
2023-03-12 11:19:50 -0700

    Removed stupid and dumb page

--- src/pages/Socials.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "../components/Navbar";
-import "../stylesheets/Socials.css"
-
-const Socials = () => {
-  <Navbar />;
-  return <div>Socials</div>;
-};
-
-export default Socials;
