40c0a097d8611ed646921f9f48f945d702dce0ff
Daniel Schott <dschott@scu.edu>
2023-03-13 12:08:07 -0700

    Made navbar responsive and added AirDJ text for tablet/desktop sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,17 @@ import { HiHome } from 'react-icons/hi'
 const Navbar = () => {
   return (
     <div className="navbar">
-            <a href="/"><img className='logo' src="src\assets\record_logo.png"/></a>
-            <div className="button-container">
-              <a href="/home"><button href="/home"><HiHome className="icon"/>     home</button></a>
-              <a href="/account"><button > <FaUserCircle className="icon"/>     my account</button></a>
-              <a href="/help"><button >about</button></a>
-              <a href="/hosting"><button className="host-button">► host</button></a>
-            </div>
-        </div>
+      <a href="/">
+        <img className='logo' src="src\assets\record_logo.png" alt="AirDJ Logo" />
+      </a>
+      <a href="/" className="title">AirDJ</a>
+      <div className="button-container">
+        <a href="/home"><button href="/home"><HiHome className="icon"/>     Home</button></a>
+        <a href="/account"><button > <FaUserCircle className="icon"/>     Account</button></a>
+        <a href="/help"><button >About</button></a>
+        <a href="/hosting"><button className="host-button">► host</button></a>
+      </div>
+    </div>
   );
 };
 
