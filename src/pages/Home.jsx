682471d47015dc8afe66f6b0fdc8fc995ece862c
Cameron Greene <cam.greene@gmail.com>
2023-03-13 12:23:44 -0700

    search bar function of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import Navbar from "../components/Navbar";
+import ChannelSearchCard from "../components/ChannelSearchCard";
+
 import "../stylesheets/Home.css";
+import {ImSearch} from 'react-icons/im'
 
 const Home = () => {
   return (
@@ -7,7 +10,53 @@ const Home = () => {
       <Navbar />
       <div className="home-container">
         <div className="dj-search">
-          <h2>DJ Search</h2>
+          <div className="search-bar-container">
+            <input type="text" placeholder="Search..."/>
+            <button className="search-button"><ImSearch/></button>
+          </div>
+          <div className="horizontal-divider"></div>
+          <div className="channel-card-display-section">
+            <ChannelSearchCard
+              name={"DanTheMan"}
+              viewercount={3}
+              playing={"rich flex"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+            <ChannelSearchCard
+              name={"Steve Lacy"}
+              viewercount={3123}
+              playing={"bad habit steve lacy"}
+            />
+          </div>
         </div>
         <div className="station-search">
           <h2>Station Search</h2>
